2bbb25175d6760e0441b9f1176333d26eb130ed6
Simon Asika <asika32764@gmail.com>
2017-06-09 21:10:00 +0800

    test http

--- build/travis/scripts/libsodium.sh
+++ build/travis/scripts/libsodium.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo add-apt-repository ppa:chris-lea/libsodium;
+sudo add-apt-repository ppa:chris-lea/libsodium -y;
 sudo echo "deb http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list;
 sudo echo "deb-src http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list;
-sudo apt-get update && sudo apt-get install libsodium-dev;
+sudo apt-get update && sudo apt-get install libsodium-dev -y;
