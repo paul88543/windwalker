130f4042079df5e5116d7623259b2940f56547b6
Simon Asika <asika32764@gmail.com>
2017-06-09 21:06:38 +0800

    test http

--- build/travis/scripts/libsodium.sh
+++ build/travis/scripts/libsodium.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-# @see https://github.com/google/hat-backup/blob/master/travis-install-libsodium.sh
-# check if libsodium is already installed
-if [ ! -d "$HOME/libsodium/lib" ]; then
-  wget https://github.com/jedisct1/libsodium/releases/download/1.0.11/libsodium-1.0.11.tar.gz
-  tar xvfz libsodium-1.0.11.tar.gz
-  cd libsodium-1.0.11
-  ./configure --prefix=$HOME/libsodium
-  make
-  make install
-else
-  echo 'Using cached directory.'
-fi
+sudo add-apt-repository ppa:chris-lea/libsodium;
+sudo echo "deb http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list;
+sudo echo "deb-src http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list;
+sudo apt-get update && sudo apt-get install libsodium-dev;
